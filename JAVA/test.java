793d20898f539674d87cc2892c3a507d84c9d7d8
MrCoww2510 <mrcoww25102005@gmail.com>
2025-10-02 05:23:45 +0700

    added

---
+++ JAVA/test.java
@@ -0,0 +1,14 @@
+package JAVA;
+
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+    }
+}
