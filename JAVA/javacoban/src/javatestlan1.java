68ed819a48f3219e936399d2ee78a1f6fd11fdc7
MrCoww2510 <mrcoww25102005@gmail.com>
2025-10-02 06:05:01 +0700

    test lan thu n

---
+++ JAVA/javacoban/src/javatestlan1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class javatestlan1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello, World!");
+        int number = scanner.nextInt();
+        System.out.println("You entered: " + number);
+        scanner.close();
+    }
+}
